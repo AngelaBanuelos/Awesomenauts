6ccb53360f89e8b82871113813da46fadde7919c
Angela <banuelosangela56@gmail.com>
2015-01-26 11:47:57 -0800

    Video 8

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -20,7 +20,7 @@ game.PlayerEntity = me.Entity.extend({
 			}
 		}]);
 		//sets the speen of the player when it moves to the right 
-		this.body.setVelocity(5,0);
+		this.body.setVelocity(5, 20);
 
 	},
 	// updates the fucntion 
