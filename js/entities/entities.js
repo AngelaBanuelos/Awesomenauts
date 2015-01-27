85e20b874f04a18bc2dc85dc0aa33f6d643fe3a0
Angela <banuelosangela56@gmail.com>
2015-01-27 10:49:19 -0800

    Coded for Video 10

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -22,6 +22,11 @@ game.PlayerEntity = me.Entity.extend({
 		//sets the speen of the player when it moves to the right 
 		this.body.setVelocity(5, 20);
 
+		this.renderable.addAnimation("idle", [78]);
+		this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
+
+		this.renderable.setCurrentAnimation("idle");
+
 	},
 	// updates the fucntion 
 update: function(delta){
@@ -30,12 +35,24 @@ update: function(delta){
 		//set velocity() and multiplying it by me.timer.tick.
 		//me.timer.tick makes the movement look smooth
 		this.body.vel.x += this.body.accel.x * me.timer.tick;
+		this.flipX(true);
 	}else{
 		//for when the rigth arrow isnt clicked 
 		this.body.vel.x = 0;
 	}
+	if(this.body.vel.x !==0 ){
+	if(!this.renderable.isCurrentAnimation("walk")){
+		this.renderable.setCurrentAnimation("walk");
+
+	}else{
+		this.renderable.setCurrentAnimation("idle");
+	}
+}
+
 	//updates the function to true
 	this.body.update(delta);
+
+	this._super(me.Entity, "update", [delta]);
 	return true;
 }
 
