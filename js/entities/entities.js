de57c0e8ac03136da5a9073a7870afac84e5e91c
Angela <banuelosangela56@gmail.com>
2015-01-27 11:04:18 -0800

    Commented for video 10

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -20,11 +20,12 @@ game.PlayerEntity = me.Entity.extend({
 			}
 		}]);
 		//sets the speen of the player when it moves to the right 
-		this.body.setVelocity(5, 20);
-
+		this.body.setVelocity( 5, 20);
+		//adds animation to standing starting position
 		this.renderable.addAnimation("idle", [78]);
+		//adds animation to walking
 		this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
-
+		//adds animation to non-moving position. 
 		this.renderable.setCurrentAnimation("idle");
 
 	},
@@ -35,23 +36,27 @@ update: function(delta){
 		//set velocity() and multiplying it by me.timer.tick.
 		//me.timer.tick makes the movement look smooth
 		this.body.vel.x += this.body.accel.x * me.timer.tick;
+		//makes the character face the right
 		this.flipX(true);
 	}else{
 		//for when the rigth arrow isnt clicked 
 		this.body.vel.x = 0;
 	}
+
+	//if the button is pushed then the character will walk but if not it will execute the else code.
 	if(this.body.vel.x !==0 ){
 	if(!this.renderable.isCurrentAnimation("walk")){
 		this.renderable.setCurrentAnimation("walk");
 
 	}else{
+		//if not, make it stand still.
 		this.renderable.setCurrentAnimation("idle");
 	}
 }
 
 	//updates the function to true
 	this.body.update(delta);
-
+	//this line of code is to update the animation
 	this._super(me.Entity, "update", [delta]);
 	return true;
 }
