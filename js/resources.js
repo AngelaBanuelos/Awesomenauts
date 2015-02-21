1b23625841b5b3566f54914f0a307c6dedc47aaf
Angela Banuelos <banuelosangela56@gmail.com>
2015-02-20 21:29:24 -0800

    Coded video 26

--- js/resources.js
+++ js/resources.js
@@ -15,7 +15,7 @@ game.resources = [
 	 {name: "creep1", type:"image", src: "data/img/brainmonster.png"},
 	 // loads second player image
 	 {name: "Player2", type:"image", src: "data/img/gloop.png"},
-		
+	{name: "title-screen", type:"image", src: "data/img/title.png"},
 	/* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
