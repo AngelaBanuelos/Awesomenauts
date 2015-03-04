c0f83bd21f5aa4739758749e531518f8e8173805
Angela Banuelos <banuelosangela56@gmail.com>
2015-03-04 10:38:40 -0800

    Coded videos 39

--- js/screens/title.js
+++ js/screens/title.js
@@ -5,32 +5,34 @@ game.TitleScreen = me.ScreenObject.extend({
 	onResetEvent: function() {
 	//adds and loads the title image	
 		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10); // TODO
-		//when the enter key is pressed 
-		me.input.bindKey(me.input.KEY.ENTER, "start");
-		//
+		
 		me.game.world.addChild(new (me.Renderable.extend({
 			init: function(){
 				//
-				this._super(me.Renderable, "init", [510, 30, me.game.viewport.width, me.game.viewport.height])
+				this._super(me.Renderable, "init", [270, 240, 300, 50])
 				//makes th efont arial and font size 46 and the color white.
 				this.font = new me.Font("Arial", 46, "white");
-
+				me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true);
 			},
 			draw: function(renderer){
 				//writes "Awesomenauts" at those specific coordinates
-				this.font.draw(renderer.getContext(), "Awesomenauts!", 450, 130);
-				//writes out "Pess enter to play" at those specific coordinates
-				this.font.draw(renderer.getContext(), "Press Enter To Play", 250, 530);
+				this.font.draw(renderer.getContext(), "Start A New Game", this.pos.x, this.pos.y);
+			},
+
+			update: function(dt){
+				return true;
+			},
+			newGame: function(){
+				me.input.releasePointerEvent('pointerdown', this);
+				me.save.remove('exp');
+				me.save.remove('exp1');
+				me.save.remove('exp2');
+				me.save.remove('exp3');
+				me.save.remove('exp4');
+				me.state.change(me.state.PLAY);
 			}
 
 		})));
-		//
-		this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCode, edge){
-			if(action === "start"){
-				me.state.change(me.state.PLAY);
-			}
-		});
-
 	},
 	
 	
@@ -38,9 +40,5 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
-		//
-		me.input.unbindKey(me.input.KEY.ENTER); // TODO
-		//
-		me.event.unsubscribe(this.handler);
-	}
+		}
 });
