ca43d49986d56643e49c288ae80aaf5099692b71
Angela Banuelos <banuelosangela56@gmail.com>
2015-03-04 10:50:59 -0800

    Commented video 39

--- js/screens/title.js
+++ js/screens/title.js
@@ -8,26 +8,35 @@ game.TitleScreen = me.ScreenObject.extend({
 		
 		me.game.world.addChild(new (me.Renderable.extend({
 			init: function(){
-				//
+				//changed the x, y, width and height
 				this._super(me.Renderable, "init", [270, 240, 300, 50])
 				//makes th efont arial and font size 46 and the color white.
 				this.font = new me.Font("Arial", 46, "white");
+				//registered the pointer to start new game
 				me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true);
 			},
 			draw: function(renderer){
 				//writes "Awesomenauts" at those specific coordinates
+				//changed the position of "Start A New Game"
 				this.font.draw(renderer.getContext(), "Start A New Game", this.pos.x, this.pos.y);
 			},
-
+			//listens for the mouse
 			update: function(dt){
 				return true;
 			},
+			//funciton to start a new game
 			newGame: function(){
+				//calls the pointerdown registered input
 				me.input.releasePointerEvent('pointerdown', this);
+				//this removes the past experience points and saves the new one
 				me.save.remove('exp');
+				//this removes the past experience points and saves the new one
 				me.save.remove('exp1');
+				//this removes the past experience points and saves the new one
 				me.save.remove('exp2');
+				//this removes the past experience points and saves the new one
 				me.save.remove('exp3');
+				//this removes the past experience points and saves the new one
 				me.save.remove('exp4');
 				me.state.change(me.state.PLAY);
 			}
